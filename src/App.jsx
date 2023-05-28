037f55df53e3bf064d8f28a506297dc4246dbe33
Meir <meirtubiana@gmail.com>
2023-05-28 22:52:26 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { AppBar, Button, Grid, Typography } from "@mui/material";
 import { theme } from "../theme";
-import { Box, Container, Stack, ThemeProvider } from "@mui/system";
+import {
+  Box,
+  Container,
+  Stack,
+  ThemeProvider,
+  borderRadius,
+} from "@mui/system";
 import "./css/default.css";
 import styled from "@emotion/styled";
 import Home from "./components/Home/Home";
@@ -10,61 +16,91 @@ import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 import Sidebar from "./components/Sidebar";
+import { useState } from "react";
 
 const PageWrapper = styled(Box)({
-    width: "100vw",
-    display: "flex",
-    justifyContent: "center",
-    overflow: "hidden",
-    position: "relative",
-})
+  width: "100vw",
+  display: "flex",
+  justifyContent: "center",
+  overflow: "hidden",
+  position: "relative",
+});
 
 const PageGrid = styled(Grid)({
   width: "70vw",
   position: "relative",
-  flexDirection: "column"
+  flexDirection: "column",
 });
 
 const OverlayBG = styled(Box)({
-    position: "fixed",
-    inset: 0,
-    backgroundImage: "url(src/assets/Bg-Noise.png)",
-    backgroundPosition: "0px 0px",
-    backgroundSize: "100% 100vh",
-})
+  position: "fixed",
+  inset: 0,
+  backgroundImage: "url(src/assets/Bg-Noise.png)",
+  backgroundPosition: "0px 0px",
+  backgroundSize: "100% 100vh",
+});
 
 function App() {
+  const handleMoveCursor = (e) => {
+    setCursorPlacing({
+      clientX: e.clientX,
+      clientY: e.clientY,
+    });
+  };
+
+  const [cursorPlacing, setCursorPlacing] = useState({
+    clientX: 0,
+    clientY: 0,
+  });
 
   return (
     // Overriding default font with theme
-    <ThemeProvider theme={theme}>
-      <Sidebar />
-      <PageWrapper bgcolor="#1C1A1B">
-        <PageGrid container id="home" sx={{ height: "100vh" }}>
-          <Home />
-        </PageGrid>
-      </PageWrapper>
-      <PageWrapper bgcolor="#161516">
-        <PageGrid container id="about" sx={{ height: "100vh" }}>
-          <About />
-        </PageGrid>
-      </PageWrapper>
-      <PageWrapper bgcolor="#111010">
-        <PageGrid container id="knowledge" sx={{ height: "100vh" }}>
-          <Knowledge />
-        </PageGrid>
-      </PageWrapper>
-      <PageWrapper bgcolor="#0b0a0b">
-        <PageGrid container id="projects" sx={{ height: "100vh" }}>
-          <Projects />
-        </PageGrid>
-      </PageWrapper>
-      <PageWrapper bgcolor="#080808">
-        <PageGrid container id="contact" sx={{ height: "100vh" }}>
-          <Contact />
-        </PageGrid>
-      </PageWrapper>
-    </ThemeProvider>
+    <div onMouseMove={(e) => handleMoveCursor(e)}>
+      <ThemeProvider theme={theme}>
+        <Box
+          component="div"
+          sx={{
+            width: 100,
+            height: 100,
+            position: "fixed",
+            zIndex: "100000",
+            background: "rgba(255, 255, 255, 0.15)",
+            transform: `translate(${cursorPlacing.clientX - 50}px, ${
+              cursorPlacing.clientY - 50
+            }px)`,
+            borderRadius: "50%",
+            pointerEvents: "none",
+            filter: "blur(50px)",
+          }}
+        ></Box>
+        <Sidebar />
+        <PageWrapper bgcolor="#1C1A1B">
+          <PageGrid container id="home" sx={{ height: "100vh" }}>
+            <Home />
+          </PageGrid>
+        </PageWrapper>
+        <PageWrapper bgcolor="#252324">
+          <PageGrid container id="about" sx={{ height: "100vh" }}>
+            <About />
+          </PageGrid>
+        </PageWrapper>
+        <PageWrapper bgcolor="#2e2c2d">
+          <PageGrid container id="knowledge" sx={{ height: "100vh" }}>
+            <Knowledge />
+          </PageGrid>
+        </PageWrapper>
+        <PageWrapper bgcolor="#373637">
+          <PageGrid container id="projects" sx={{ height: "100vh" }}>
+            <Projects />
+          </PageGrid>
+        </PageWrapper>
+        <PageWrapper bgcolor="#414040">
+          <PageGrid container id="contact" sx={{ height: "100vh" }}>
+            <Contact />
+          </PageGrid>
+        </PageWrapper>
+      </ThemeProvider>
+    </div>
   );
 }
 
