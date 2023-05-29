8967c66f7dc7f8a90121941307dd86109744fb42
Meir <meirtubiana@gmail.com>
2023-05-29 12:09:41 +0300

    memory efficient code to split page wraps

--- src/App.jsx
+++ src/App.jsx
@@ -10,29 +10,34 @@ import About from "./components/About/About";
 import Contact from "./components/Contact/Contact";
 import Sidebar from "./components/Sidebar";
 import { Parallax, ParallaxProvider } from "react-scroll-parallax";
+import PageWrapper from "./components/PageWrapper";
 
+// PageItems [{}...]
 
-const PageWrapper = styled(Box)({
-  width: "100vw",
-  display: "flex",
-  justifyContent: "center",
-  overflow: "hidden",
-  position: "relative",
-});
-
-const PageGrid = styled(Grid)({
-  width: "70vw",
-  position: "relative",
-  flexDirection: "column",
-});
-
-const OverlayBG = styled(Box)({
-  position: "fixed",
-  inset: 0,
-  backgroundImage: "url(src/assets/Bg-Noise.png)",
-  backgroundPosition: "0px 0px",
-  backgroundSize: "100% 100vh",
-});
+const pageItems = [
+    {
+        ref: "home",
+        bgcolor: null,
+        page: <Home />
+    },
+    {
+        ref: "about",
+        bgcolor: null,
+        page: <About />
+    },
+    {
+        ref: "knowledge",
+        bgcolor: <Knowledge />
+    },
+    {
+        ref: "projects",
+        bgcolor: <Projects />
+    },
+    {
+        ref: "contact",
+        bgcolor: <Contact />
+    },
+];
 
 function App() {
 
@@ -40,31 +45,9 @@ function App() {
     // Overriding default font with theme
     <ThemeProvider theme={theme}>
       <Sidebar />
-      <PageWrapper bgcolor="#1C1A1B">
-        <PageGrid container id="home" sx={{ height: "100vh" }}>
-          <Home />
-        </PageGrid>
-      </PageWrapper>
-      <PageWrapper bgcolor="#2B1763">
-        <PageGrid container id="about" sx={{ height: "100vh" }}>
-          <About />
-        </PageGrid>
-      </PageWrapper>
-      <PageWrapper bgcolor="#184346">
-        <PageGrid container id="knowledge" sx={{ height: "100vh" }}>
-          <Knowledge />
-        </PageGrid>
-      </PageWrapper>
-      <PageWrapper bgcolor="#373637">
-        <PageGrid container id="projects" sx={{ height: "100vh" }}>
-          <Projects />
-        </PageGrid>
-      </PageWrapper>
-      <PageWrapper bgcolor="#414040">
-        <PageGrid container id="contact" sx={{ height: "100vh" }}>
-          <Contact />
-        </PageGrid>
-      </PageWrapper>
+        {pageItems.map((item, index) => (
+            <PageWrapper item={item} key={index}/>
+        ))}
     </ThemeProvider>
   );
 }
---
+++ src/components/PageWrapper.jsx
@@ -0,0 +1,30 @@
+import styled from "@emotion/styled";
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
+
+const PageGrid = styled(Grid)({
+  width: "70vw",
+  position: "relative",
+  flexDirection: "column",
+});
+
+const PageWrapper = ({ item }) => {
+    console.log(item)
+    return (
+      <Box
+        sx={{
+          width: "100vw",
+          display: "flex",
+          justifyContent: "center",
+          overflow: "hidden",
+          position: "relative",
+        }}
+      >
+        <PageGrid container id={item.ref} sx={{ height: "100vh" }}>
+          {item.page}
+        </PageGrid>
+      </Box>
+    );
+}
+ 
+export default PageWrapper;
